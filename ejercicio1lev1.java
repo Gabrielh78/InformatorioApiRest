58dcd2cd3369cdefb3a63cad6105c6f8b27d3cd6
Gabrielh78 <68515945+Gabrielh78@users.noreply.github.com>
2020-12-08 16:32:42 -0300

    Add files via upload

---
+++ ejercicio1lev1.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class ejercicio1lev1 {
+    public static void main(String[] args) {
+        String cadena;
+        cadena = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        JOptionPane.showMessageDialog(null,"Bienvenido " + cadena);
+    }
+
+}
